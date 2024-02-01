73ae1bd6fb11b72d9c64e930135771ba2c412990
Suresh Murugasamy <91257119+White-Devil-27@users.noreply.github.com>
2024-02-01 10:29:21 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -20,13 +20,13 @@ export default function Works(){
                             <p className="--works-info">An Animative Portfolio Web App built with React, CSS and JavaScript to visualizing my featured Works and Skills.</p>
                         </div>
                     </div>
-                    <div className="col-sm-6 --works-img"  data-aos="fade-up" data-aos-duration="2200">
+{/*                     <div className="col-sm-6 --works-img"  data-aos="fade-up" data-aos-duration="2200">
                         <a href="https://sureshmurugasamy-tenzi.netlify.app/" target="_blank" rel="noreferrer"><img src={tenzi} alt="" /></a>
                         <div className="text-center" style={{background: "#112240"}}>
                             <h4 className="--works-head">TENZI GAME</h4>
                             <p className="--works-info1">It's a small Game WebApplication I re-built with React hooks for understanding purpose.</p>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </React.Fragment>
